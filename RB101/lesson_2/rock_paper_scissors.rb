bbc5f949ed5461706f4a34be8c001b0e7257bb4f
Chris Prigmore <chris.prigmore@gmail.com>
2021-02-17 19:52:23 -0800

    rock paper scissors

---
+++ RB101/lesson_2/rock_paper_scissors.rb
@@ -0,0 +1,49 @@
+# Rock Paper Scissors game
+VALID_CHOICES = %w(rock paper scissors)
+
+puts VALID_CHOICES.inspect
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    puts("error")
+  end
+end
+
+choice = ''
+
+loop do
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That is not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?(y/n)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for playing!")
