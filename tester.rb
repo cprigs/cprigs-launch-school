a0544fc7ad45eccd8ba19c216a184f68068d94c7
Chris Prigmore <chris.prigmore@gmail.com>
2021-09-04 14:16:47 -0700

    re-organizing

--- tester.rb
+++ tester.rb
@@ -1,10 +1,23 @@
-def string_lengths(sentence)
-  strings = sentence.split
-  lengths = []
+#def string_lengths(sentence)
+#  strings = sentence.split
+#  lengths = []
 
-  strings.each do |string|
-    lengths << string.size
-  end
+#  strings.each do |string|
+#    lengths << string.size
+#  end
+#end
+
+#puts string_lengths("a bird is yellow")
+
+
+def name
+  "George"
 end
 
-puts string_lengths("a bird is yellow")
+name = "Lisa"
+
+
+loop do
+  puts name
+  break
+end
